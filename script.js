b8f6254679a81c1f1c72280a001603da2d5dddf3
moulikjindal <100513188+moulikjindal@users.noreply.github.com>
2023-10-18 14:24:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+window.embeddedChatbotConfig = {
+chatbotId: "rIU1cjDBoF5ZmaLOnUruK",
+domain: "www.chatbase.co"
+}
+</script>
+<script
+src="https://www.chatbase.co/embed.min.js"
+chatbotId="rIU1cjDBoF5ZmaLOnUruK"
+domain="www.chatbase.co"
+defer>
+</script>
